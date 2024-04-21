8a5f1271b10adaa9ae534f13273af29167a6b9bf
mike6321 <mike6321@naver.com>
2024-04-21 20:06:11 +0900

    feat : docker-compose 환경 서비스 연동

--- CatalogService/src/main/java/com/example/catalogservice/dto/ProductTagsDto.java
+++ CatalogService/src/main/java/com/example/catalogservice/dto/ProductTagsDto.java
@@ -1,9 +1,11 @@
 package com.example.catalogservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
 public class ProductTagsDto {
 
--- OrderService/src/main/java/com/example/orderservice/feign/CatalogClient.java
+++ OrderService/src/main/java/com/example/orderservice/feign/CatalogClient.java
@@ -4,7 +4,7 @@ import com.example.orderservice.dto.DecreaseStockCountDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
@@ -15,7 +15,7 @@ public interface CatalogClient {
     @GetMapping(value = "/catalog/products/{productId}")
     Map<String, Object> getProduct(@PathVariable Long productId);
 
-    @PostMapping(value = "/catalog/products/{productId}/decreaseStockCount")
+    @PutMapping(value = "/catalog/products/{productId}/decreaseStockCount")
     void decreaseStockCount(@PathVariable Long productId, @RequestBody DecreaseStockCountDto dto);
 
 }
